3a7d57b375394c4ce7dd050a469650b8d1fe944c
andru196 <andru196@icloud.com>
2021-10-20 18:35:50 +0300

    aga

--- download_data.sh
+++ download_data.sh
@@ -1,10 +1,18 @@
-kaggle competitions download -c sberbank-russian-housing-market -p data/raw
-unzip data/raw/sberbank-russian-housing-market.zip -d data/raw
-rm data/raw/sberbank-russian-housing-market.zip
+RAW="data/raw"
+LOG="download.log"
+rm -rf $RAW
+date >> $LOG
+kaggle competitions download -c sberbank-russian-housing-market -p $RAW >> $LOG
+unzip data/raw/sberbank-russian-housing-market.zip -d $RAW >> $LOG
+rm data/raw/sberbank-russian-housing-market.zip >> $LOG
 FILES="data/raw/*.zip"
 for f in $FILES
 do
-  echo $f
-  unzip $f -d data/raw
-  rm $f
+	unzip $f -d data/raw >> $LOG
+	rm $f >> $LOG
+	if [ $? -eq 0 ]
+	then
+		echo $f deleted
+	fi
 done
+echo "HOMEWORK IN notebooks/HW1.ipynb"
